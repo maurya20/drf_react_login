ae145bffe625ae9d6b68b93c5045cb42ba837f2b
maurya20 <mukesh.ice17@gmail.com>
2020-12-10 17:25:55 +0530

    token obtained at http://127.0.0.1:8000/api/token/

--- server/server/urls.py
+++ server/server/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include('mysite.urls')),
     path("api/auth/", include('rest_auth.urls')),
-    path("api/token/", obtain_jwt_token),
+    path("api/token/", obtain_jwt_token),   # For login getting JWT
    
 ]
